3cb9409494a21078a0f747abff6de595a98712c9
Charlie Phan <charliegphan@gmail.com>
2019-01-08 18:51:34 -0800

    add reduce solution

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,21 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-  let reversed = '';
-  // for (let i = str.length - 1; i >= 0; i--) {
-  //   reversed += str[i];
-  // }
+// function reverse(str) {
+//   let reversed = '';
+//   // for (let i = str.length - 1; i >= 0; i--) {
+//   //   reversed += str[i];
+//   // }
 
-  for (let character of str) {
-    reversed = character + reversed;
-  }
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
 
-  return reversed;
-}
+//   return reversed;
+// }
 
 // function reverse(str) {
 //   return str.split('').reverse().join('');
 // }
+
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
+}
 
 module.exports = reverse;
