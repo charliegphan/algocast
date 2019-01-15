f1496e5f8cb06cdaab3c77f500881617ab91b786
Charlie Phan <charliegphan@gmail.com>
2019-01-14 17:14:26 -0800

    use for of loop instead

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,26 +5,45 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {
-  const charFreq = {};
-  for (let i = 0; i < str.length; i++) {
-    if (charFreq[str[i]]) {
-      charFreq[str[i]]++
-    } else {
-      charFreq[str[i]] = 1;
-    }
-  }
+// function maxChar(str) {
+//   const charFreq = {};
+//   for (let i = 0; i < str.length; i++) {
+//     if (charFreq[str[i]]) {
+//       charFreq[str[i]]++
+//     } else {
+//       charFreq[str[i]] = 1;
+//     }
+//   }
   
-  let freq = 0;
-  let max;
-  for (let key in charFreq) {
-    if (charFreq[key] > freq) {
-      freq = charFreq[key];
-      max = key;
+//   let freq = 0;
+//   let max;
+//   for (let key in charFreq) {
+//     if (charFreq[key] > freq) {
+//       freq = charFreq[key];
+//       max = key;
+//     }
+//   }
+
+//   return max;
+// }
+
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      marChar = char;
     }
   }
 
-  return max;
+  return marChar;
 }
 
 module.exports = maxChar;
