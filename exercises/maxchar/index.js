35055c6e01b0928ecfe959ed0fc39f4ca89f7aa7
Charlie Phan <charliegphan@gmail.com>
2019-01-14 16:04:41 -0800

    complete maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charFreq = {};
+  for (let i = 0; i < str.length; i++) {
+    if (charFreq[str[i]]) {
+      charFreq[str[i]]++
+    } else {
+      charFreq[str[i]] = 1;
+    }
+  }
+  
+  let max;
+  for (let key in charFreq) {
+    if (charFreq[key] > max) {
+      max = charFreq[key];
+    }
+  }
+  
+  return max;
+}
 
 module.exports = maxChar;
